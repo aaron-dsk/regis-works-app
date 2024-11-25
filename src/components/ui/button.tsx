5f40607359c670c68b9e9128a1e940efdb3eb0ca
aaron-dsk <aaron@dsklabs.com>
2024-11-25 15:26:46 -0300

    Changed: hover effect in the sidebar nav menu

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "border outline-blue bg-background shadow-sm bg-white hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-gray-200 hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
